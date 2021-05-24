1671d84ba6b0af56b6c3be6d19048d45b131cba3
jackcusick95 <jackcusick95@gmail.com>
2021-05-24 16:56:41 -0400

    finished event post route

--- app.js
+++ app.js
@@ -17,14 +17,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 //parse json sent to frontend
 
-<<<<<<< HEAD
-// app.get("/", (req, res) => res.send("test1234"));
-=======
 app.use(passport.initialize());
 require("./config/passport")(passport);
 
 
->>>>>>> main
 app.use("/api/users", users);
 app.use("/api/events", events);
 // //backend api/users route
--- routes/api/events.js
+++ routes/api/events.js
@@ -13,12 +13,6 @@ router.post('/create',
     passport.authenticate('jwt', { session: false }),
     (req, res) => {
     
-    // const { errors, isValid} = validateEventInput(req.body);
-
-    // if (!isValid) {
-    //     return res.status(400).json(errors);
-    // }
-
         const newEvent = new Event({
             location_id: req.body.location_id,
             user_id: req.user.id,
