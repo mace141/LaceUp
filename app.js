661e22daa9ccbd3901293f3451d9ebf796519b7c
akerpelm <akerpelm@u.rochester.edu>
2021-05-24 10:22:35 -0400

    complete registration functionality

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
-app.user(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 //parse json sent to frontend
 
 
 app.get("/", (req, res) => res.send("test1234"));
 app.use("/api/users", users);
-//backend api/users route
+// //backend api/users route
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on ${port}`));
