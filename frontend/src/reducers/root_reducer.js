7ae01eb280af7de64fc45683ef6f61bcb28dda30
Daniel <danielwu116@gmail.com>
2021-05-24 14:55:57 -0400

    Add entities reducer to root reducer, users reducer to entities reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import session from "./session_reducer";
 import errors from "./errors_reducer";
+import entitiesReducer from "./entities";
 
 const RootReducer = combineReducers({
+  entities: entitiesReducer,
   session,
   errors,
 });
