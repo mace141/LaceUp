d04e12de5150edc690943a1b3e772ae52111c113
jackcusick95 <jackcusick95@gmail.com>
2021-05-24 09:46:53 -0400

    Rename key_dev folder and put it on the gitignore

--- config/keys_dev_delete_later.js
+++
@@ -1,7 +0,0 @@
-// copy contents below into a new file called keys_dev and delete file
-
-module.exports = {
-  mongoURI:
-    "mongodb+srv://admin:xsFHS0QnqzilzRSt@cluster0.e9sgg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  secretOrKey: "skv7nXueu8T3ELhC2",
-};
