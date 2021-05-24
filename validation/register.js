e8567adf0a1f1c1f2adb584419348c37bf017a48
akerpelm <akerpelm@u.rochester.edu>
2021-05-24 13:41:36 -0400

    continue validations

--- validation/register.js
+++ validation/register.js
@@ -29,10 +29,33 @@ module.exports = function validateRegisterInput(data) {
   if (!Validator.isEmail(data.email)) {
     errors.email = "Please enter a valid email";
   }
+//   possibly normalize email
   if (Validator.isEmpty(data.fname)) {
     errors.fname = "An first name is required";
   }
   if (Validator.isEmpty(data.lname)) {
-    errors.lname = "An lastname is required";
+    errors.lname = "An last name is required";
+  }
+  //can use strong password validator
+  if(!Validator.isLength(data.password, {min: 6, max: 50})) {
+      errors.password = 'Password must contain at least 6 characters'
+  }
+  if(Validator.isEmpty(data.password)) {
+      errors.password = "A password is required"
+  }
+
+  if (Validator.isEmpty(data.password2)) {
+      errors.password2 = "Must confirm password"
+  }
+  if (!Validator.equals(data.password, data.password2)) {
+      errors.password2 = "Passwords must match"
+  }
+  if(!Validator.isLength(data.bio), {min: 1, max: undefined}) {
+      errors.bio = "Bio must contain at least one "
+  }
+
+  return {
+      errors,
+      isValid = Object.keys(errors).length === 0
   }
 };
