fd6be8b537fefd4237d852091df2af8768e90e27
Daniel <danielwu116@gmail.com>
2021-05-24 14:41:02 -0400

    Add updateUser API method, fetchUser & updateUser action creators, usersReducer

--- frontend/src/util/user_api.js
+++ frontend/src/util/user_api.js
@@ -4,3 +4,8 @@ export const fetchUser = userId => {
   return axios.get(`/api/users/${userId}`)
 };
 
+export const updateUser = formData => {
+  return axios.post(`/api/users/${formData.get('user[id]')}`, formData, {
+    headers: formData.getHeaders()
+  })
+};
