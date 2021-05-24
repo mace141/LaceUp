12bb51ef71cbfa2cf1bc0395efeb452a04e4574c
Andrew Kihs <ajkihs@gmail.com>
2021-05-23 21:45:24 -0400

    Build out frontend skeleton

---
+++ config/keys_dev_delete_later.js
@@ -0,0 +1,7 @@
+// copy contents below into a new file called keys_dev and delete file
+
+module.exports = {
+  mongoURI:
+    "mongodb+srv://<username>:<password>@app-name.mroze.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  secretOrKey: "secret_key_hash",
+};
---
+++ frontend/src/components/app.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { AuthRoute, ProtectedRoute } from "../util/route_util";
+import { Switch } from "react-router-dom";
+
+// import MainPage from "./main/main_page";
+// import NavBarContainer from "./nav/navbar_container";
+// import LoginFormContainer from "./user_auth/login_form_container";
+// import SignupFormContainer from "./user_auth/signup_form_container";
+
+const App = () => (
+  <div>
+    {/* <NavBarContainer /> */}
+    <Switch>
+      {/* <AuthRoute exact path="/" component={MainPage} />
+      <AuthRoute exact path="/login" component={LoginFormContainer} />
+      <AuthRoute exact path="/signup" component={SignupFormContainer} /> */}
+    </Switch>
+  </div>
+);
+
+export default App;
