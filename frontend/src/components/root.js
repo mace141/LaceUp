b2d699c744be83d16e630ee5aa577955a588d728
Andrew Kihs <ajkihs@gmail.com>
2021-05-24 00:32:07 -0400

    Delete init favicon

--- frontend/src/components/root.js
+++ frontend/src/components/root.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
-// We'll create this soon
 import App from "./app";
+
+import "../styles/main.scss";
 
 const Root = ({ store }) => (
   <Provider store={store}>
