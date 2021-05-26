a9d7abb6def4d23d1a292ac24e362b1f75a33f98
Andrew Kihs <ajkihs@gmail.com>
2021-05-26 15:58:36 -0400

    Fix passing of props to components

--- frontend/src/components/explore/event_explore/event_index_container.js
+++ frontend/src/components/explore/event_explore/event_index_container.js
@@ -2,9 +2,21 @@ import { connect } from "react-redux";
 import EventIndex from "./event_index";
 import { fetchParksEvents } from "../../../actions/event";
 const mapStateToProps = (state, ownProps) => {
+  // debugger;
+
+  const eventState = state.entities.events;
+
+  const filteredEvents = Object.fromEntries(
+    Object.entries(eventState).filter(
+      ([key, value]) => value.location_id === ownProps.park._id
+    )
+  );
+  // debugger;
+
   return {
     park: ownProps.park,
     errors: state.errors.event,
+    parksEvents: filteredEvents,
   };
 };
 
---
+++ frontend/src/components/explore/event_explore/event_index_item.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { withRouter } from "react-router-dom";
+
+class EventIndexItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    const { event, fetchUser } = this.props;
+    fetchUser(event.user_id);
+  }
+
+  render() {
+    const { event, park, host } = this.props;
+
+    const months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    const date = new Date(event.date);
+
+    const month = months[date.getMonth()];
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+
+    const time = `${hours}:${minutes}`;
+
+    if (!host) {
+      return null;
+    } else {
+      return (
+        <>
+          <div className="explore-index-item">
+            <div className="date-time">
+              <p>
+                Date/Time: <span>{`${time}, ${month} ${day}, ${year}`}</span>
+              </p>
+            </div>
+            <div className="location">
+              <p>
+                Location: <span>{park.name}</span>
+              </p>
+            </div>
+            <div className="sport">
+              <p>
+                Sport: <span>{event.sport}</span>
+              </p>
+            </div>
+            <div className="skill">
+              <p>
+                Skill level: <span>{event.skill}</span>
+              </p>
+            </div>
+            <div className="team-size">
+              <p>
+                Team Size: <span>{event.team_size}</span>
+              </p>
+            </div>
+            <div className="link-to-event">
+              <button className="join-game-btn">
+                <Link to={`/events/${event._id}`}>Join game</Link>
+              </button>
+            </div>
+          </div>
+        </>
+      );
+    }
+  }
+}
+
+export default withRouter(EventIndexItem);
