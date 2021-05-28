3c75e7742329c9fa76f7b857eb438bc5d3ba13ff
akerpelm <akerpelm@u.rochester.edu>
2021-05-27 21:14:14 -0400

    begin AWS functionality

--- routes/api/users.js
+++ routes/api/users.js
@@ -203,4 +203,74 @@ router.get("/:id", (req, res) => {
     .catch((err) => res.status(404).json(err));
 });
 
+const aws = require("aws-sdk");
+const multerS3 = require("multer-s3");
+const multer = require("multer");
+const path = require("path");
+const url = require("url");
+let AWS = require("../../config/keys_dev")
+
+const profileImgUpload = multer({
+  storage: multerS3({
+    s3: AWS.s3,
+    bucket: "ak-laceup-bucket",
+    acl: "public-read",
+    key: function (req, file, cb) {
+      cb(
+        null,
+        path.basename(file.originalname, path.extname(file.originalname)) +
+          "-" +
+          Date.now() +
+          path.extname(file.originalname)
+      );
+    },
+  }),
+  limits: { fileSize: 2000000 },
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+}).single("image");
+
+
+function checkFileType(file, cb) {
+  // Allowed ext
+  const filetypes = /jpeg|jpg|png|gif/;
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb("Error: Images Only!");
+  }
+}
+
+router.post("/test-img", (req, res) => {
+  profileImgUpload(req, res, (error) => {
+    // console.log( 'requestOkokok', req.file );
+    // console.log( 'error', error );
+    if (error) {
+      console.log("errors", error);
+      res.json({ error: error });
+    } else {
+      // If File not found
+      if (req.file === undefined) {
+        console.log("Error: No File Selected!");
+        res.json("Error: No File Selected");
+      } else {
+        // If Success
+        const imageName = req.file.key;
+        // const imageName = req.body.image;
+        const imageLocation = req.file.location;
+        // Save the file name into database into profile model
+        res.json({
+          image: imageName,
+          location: imageLocation,
+        });
+      }
+    }
+  });
+});
+
 module.exports = router;
