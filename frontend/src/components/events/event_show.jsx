5f9c312fb753389fb75d636c113f5c569f469703
Daniel <danielwu116@gmail.com>
2021-05-28 10:17:08 -0400

    Add links to user profile from event show

--- frontend/src/components/events/event_show.jsx
+++ frontend/src/components/events/event_show.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 import { receiveEvent } from '../../actions/event_actions';
 import { fetchParks } from '../../actions/park';
 import { fetchTeams } from '../../actions/team';
@@ -100,7 +100,10 @@ class EventShow extends React.Component {
             </p>
           </div>
           <div className='event-host'>
-            <p>Hosted by: <span>{`${this.state.event.user_id.fname} ${this.state.event.user_id.lname}`}</span></p>
+            <p>Hosted by: <Link to={`/users/${this.state.event.user_id._id}`}>
+            <span>{`${this.state.event.user_id.fname} ${this.state.event.user_id.lname}`}</span>
+            </Link>
+            </p>
           </div>
         </div>
         <div>
