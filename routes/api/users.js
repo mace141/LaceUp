a3e895ff6164fa3ac908a649b8867f7245366384
akerpelm <akerpelm@u.rochester.edu>
2021-06-09 10:29:45 -0400

    Refactor AWS code, add image_upload directory, create /user/:id/image route

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,6 +7,8 @@ const keys = require("../../config/keys");
 const passport = require("passport");
 const { db } = require("../../models/User");
 const { MongoClient, ObjectID } = require("mongodb");
+const upload = require("../../image_upload/image_upload");
+const deleteImage = require("../../image_upload/image_delete");
 
 const User = require("../../models/User");
 const Event = require("../../models/Event");
@@ -151,66 +153,7 @@ router.patch(
     );
   }
 );
-//WIP
 
-// router.patch(
-//   "/:id",
-//   passport.authenticate("jwt", { session: false }),
-//   async (req, res) => {
-//     const { errors, isValid } = validateUpdateInput(req.body);
-
-//     if (!isValid) {
-//       return res.status(400).json(errors);
-//     }
-//     // async
-//     // (req, res) => {
-//     // let user = await User.findById(req.params.id);
-//     // // let newAv;
-//     // profileImgUpload(req, res, (error) => {
-//     //   if (error) {
-//     //     console.log("errors", error);
-//     //     res.json({ error: error });
-//     //   } else {
-//     //     if (req.file === undefined) {
-//     //       res.json("No file selected!");
-//     //     }
-//     //   }
-//     //   // user.avatar = req.file.location
-//     //   // user.save().then(user => res.json(user))
-//     //   res.json(req.file.location).then((url) => {
-//     //     user.avatar = url;
-//     //     user.save();
-//     //   });
-//     //   // user.save().then(user => res.json(user))
-//     // });
-
-//     // const { errors, isValid } = validateUpdateInput(req.body);
-
-//     // if (!isValid) {
-//     //   return res.status(400).json(errors);
-//     // }
-//     User.findByIdAndUpdate(
-//       { _id: req.params.id },
-//       {
-//         username: req.body.username,
-//         fname: req.body.fname,
-//         lname: req.body.lname,
-//         email: req.body.email,
-//         bio: req.body.bio,
-//         // home_court: body.home_court.id,
-//         favorite_sports: req.body.favorite_sports,
-//         // avatar: imageLocation,
-//       },
-//       { new: true },
-//       function (err, result) {
-//         if (err) {
-//           res.json(err);
-//         }
-//         res.json(result);
-//       }
-//     );
-//   }
-// );
 
 router.get(
   "/current",
@@ -232,125 +175,24 @@ router.get("/:id", (req, res) => {
     .catch((err) => res.status(404).json(err));
 });
 
-// const aws = require("aws-sdk");
-// const multerS3 = require("multer-s3");
-// const multer = require("multer");
-// const path = require("path");
-// const url = require("url");
-// // let AWS = require("../../config/keys_dev");
+router.post("/:id/image", upload.single("image"), async (req, res) => {
+  const currentUser = await User.findById(req.params.id);
+  if (currentUser.avatar) {
+    let imageUrl = currentUser.avatar;
+    let bucket = imageUrl.split("/")[2].split(".")[0];
+    let key = imageUrl.split("/")[3];
+    deleteImage(bucket, key)
+  }
 
-// const profileImgUpload = multer({
-//   storage: multerS3({
-//     s3: AWS.s3,
-//     bucket: "ak-laceup-bucket",
-//     acl: "public-read",
-//     key: function (req, file, cb) {
-//       cb(
-//         null,
-//         path.basename(file.originalname, path.extname(file.originalname)) +
-//           "-" +
-//           Date.now() +
-//           path.extname(file.originalname)
-//       );
-//     },
-//   }),
-//   limits: { fileSize: 2000000 },
-//   fileFilter: function (req, file, cb) {
-//     checkFileType(file, cb);
-//   },
-// }).single("image");
+  currentUser.avatar = req.file.location;
+  currentUser
+    .save()
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((e) => {
+      res.status(400).json(err);
+    });
+});
 
-// function checkFileType(file, cb) {
-//   // Allowed ext
-//   const filetypes = /jpeg|jpg|png|gif/;
-//   // Check ext
-//   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-//   // Check mime
-//   const mimetype = filetypes.test(file.mimetype);
-//   if (mimetype && extname) {
-//     return cb(null, true);
-//   } else {
-//     cb("Error: Images Only!");
-//   }
-// }
-
-// router.put("/:id/avatar", async(req, res) => {
-//   profileImgUpload(req, res, (error) => {
-//     if (error) {
-//       console.log("errors", error);
-//       res.json({ error: error });
-//     } else {
-//       // If File not found
-//       if (req.file === undefined) {
-//         console.log("Error: No File Selected!");
-//         res.json("Error: No File Selected");
-//       } else {
-//         let user = User.findBy(req.params.id)
-
-//         if (user.avatar) {
-//           let currentAvatar = currentUser.avatar;
-//           let newAvatar = req.file.location;
-//           user.avatar = newAvatar
-//         }  else {
-//           let newAvatar = req.file.location;
-//           user.avatar = newAvatar
-//         }
-//       }
-//     }
-//   }
-//   )
-// }
-// )
-
-// router.put("/:id/avatar", async (req, res) => {
-//   let user = await User.findById(req.params.id);
-//   profileImgUpload(req, res, (error) => {
-//     if (error) {
-//       console.log("errors", error);
-//       res.json({ error: error });
-//     } else {
-//       if (req.file === undefined) {
-//         res.json("No file selected!");
-//         // } else {
-//       }
-//       let newAv = req.file.location;
-//       user.avatar = newAv;
-//       user
-//         .save()
-//         .then((user) => {
-//           res.json(user);
-//         })
-//         .catch((e) => {
-//           res.status(400).json(e);
-//         });
-//       // }
-//     }
-//   });
-// });
-
-// to add to update route
-
-// passport.authenticate("jwt", { session: false }),
-// async
-// (req, res) => {
-// let user = await User.findById(req.params.id);
-// // let newAv;
-// profileImgUpload(req, res, (error) => {
-//   if (error) {
-//     console.log("errors", error);
-//     res.json({ error: error });
-//   } else {
-//     if (req.file === undefined) {
-//       res.json("No file selected!");
-//       }
-//     }
-//     user.avatar = req.file.location
-//     user.save().then(user => res.json(user))
-//   });
-
-// const { errors, isValid } = validateUpdateInput(req.body);
-
-// if (!isValid) {
-//   return res.status(400).json(errors);
-// }
 module.exports = router;
