a1d7ebdc44d038b0244cdf8ce7452747a49f444a
Daniel <danielwu116@gmail.com>
2021-06-10 15:26:34 -0400

    Fix keys s3 prod

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,10 +1,14 @@
+const aws = require("aws-sdk");
+
 module.exports = {
   mongoURI: process.env.mongoURI,
   secretOrKey: process.env.SECRET_OR_KEY,
 };
 
-module.exports.s3 = {
+const s3 = new aws.S3({
   accessKeyId: process.env.accessKeyId,
   secretAccessKey: process.env.secretAccessKey,
   Bucket: process.env.Bucket,
-}
+});
+
+module.exports.s3 = s3;
