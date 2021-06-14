cd258d321a274bc508602f2330476ab8ce0743a0
Andrew Kihs <ajkihs@gmail.com>
2021-06-14 12:05:42 -0400

    Add grid styling to founder container

--- frontend/src/components/splash/splash.js
+++ frontend/src/components/splash/splash.js
@@ -60,108 +60,116 @@ class Splash extends React.Component {
           </CarouselProvider>
         </div>
         <div className="splash-under">
+          <h1 className="founders">Founders:</h1>
           <div className="splash-footer">
-            <h1 className="founders">Founders:</h1>
             {/* <img className="footer-img" src="pic_trulli.jpg" alt="Daniel propic"></img> */}
-            <img className="footer-img" src={daniel} alt="daniel propic"></img>
-            <div className="founder-info">
-              <p className="footer-name">Daniel Wu</p>
-              <a
-                className="footer-linkedin-git"
-                href="https://www.linkedin.com/in/daniel-wu-2995a6140/"
-                target="_blank"
-              >
-                LinkedIn
-              </a>
-              <a
-                className="footer-linkedin-git"
-                href="https://github.com/mace141"
-                target="_blank"
-              >
-                Github
-              </a>
-              <a
-                className="footer-site"
-                href="https://www.google.com/"
-                target="_blank"
-              >
-                Personal Site
-              </a>
+            <div className="founder-container">
+              <img className="footer-img" src={daniel} alt="daniel propic" />
+              <div className="founder-info">
+                <p className="footer-name">Daniel Wu</p>
+                <a
+                  className="footer-linkedin-git"
+                  href="https://www.linkedin.com/in/daniel-wu-2995a6140/"
+                  target="_blank"
+                >
+                  LinkedIn
+                </a>
+                <a
+                  className="footer-linkedin-git"
+                  href="https://github.com/mace141"
+                  target="_blank"
+                >
+                  Github
+                </a>
+                <a
+                  className="footer-site"
+                  href="https://www.google.com/"
+                  target="_blank"
+                >
+                  Personal Site
+                </a>
+              </div>
             </div>
-            <img className="footer-img" src={alex} alt="alex propic"></img>
-            <div className="founder-info">
-              <p className="footer-name">Alex Kerpelman</p>
-              <a
-                className="footer-linkedin-git"
-                href="https://www.linkedin.com/in/alexander-kerpelman-22587584/"
-                target="_blank"
-              >
-                LinkedIn
-              </a>
-              <a
-                className="footer-linkedin-git"
-                href="https://github.com/akerpelm"
-                target="_blank"
-              >
-                Github
-              </a>
-              <a
-                className="footer-site"
-                href="https://www.google.com/"
-                target="_blank"
-              >
-                Personal Site
-              </a>
+            <div className="founder-container">
+              <img className="footer-img" src={alex} alt="alex propic" />
+              <div className="founder-info">
+                <p className="footer-name">Alex Kerpelman</p>
+                <a
+                  className="footer-linkedin-git"
+                  href="https://www.linkedin.com/in/alexander-kerpelman-22587584/"
+                  target="_blank"
+                >
+                  LinkedIn
+                </a>
+                <a
+                  className="footer-linkedin-git"
+                  href="https://github.com/akerpelm"
+                  target="_blank"
+                >
+                  Github
+                </a>
+                <a
+                  className="footer-site"
+                  href="https://www.google.com/"
+                  target="_blank"
+                >
+                  Personal Site
+                </a>
+              </div>
             </div>
-            <img className="footer-img" src={andrew} alt="andrew propic"></img>
-            <div className="founder-info">
-              <p className="footer-name">Andrew Kihs</p>
-              <a
-                className="footer-linkedin-git"
-                href="https://www.linkedin.com/in/andrew-kihs"
-                target="_blank"
-              >
-                LinkedIn
-              </a>
-              <a
-                className="footer-linkedin-git"
-                href="https://github.com/andrewkihs"
-                target="_blank"
-              >
-                Github
-              </a>
-              <a
-                className="footer-site"
-                href="https://www.google.com/"
-                target="_blank"
-              >
-                Personal Site
-              </a>
+            <div className="founder-container">
+              <img className="footer-img" src={andrew} alt="andrew propic" />
+              <div className="founder-info">
+                <p className="footer-name">Andrew Kihs</p>
+                <a
+                  className="footer-linkedin-git"
+                  href="https://www.linkedin.com/in/andrew-kihs"
+                  target="_blank"
+                >
+                  LinkedIn
+                </a>
+                <a
+                  className="footer-linkedin-git"
+                  href="https://github.com/andrewkihs"
+                  target="_blank"
+                >
+                  Github
+                </a>
+                <a
+                  className="footer-site"
+                  href="https://www.google.com/"
+                  target="_blank"
+                >
+                  Personal Site
+                </a>
+              </div>
             </div>
-            <img className="footer-img" src={propic} alt="jack propic"></img>
-            <div className="founder-info-jack">
-              <p className="footer-name">Jack Cusick</p>
-              <a
-                className="footer-linkedin-git"
-                href="https://www.linkedin.com/in/jack-cusick-2a5809b4/"
-                target="_blank"
-              >
-                LinkedIn
-              </a>
-              <a
-                className="footer-linkedin-git"
-                href="https://github.com/jackcusick95"
-                target="_blank"
-              >
-                Github
-              </a>
-              <a
-                className="footer-site"
-                href="https://www.google.com/"
-                target="_blank"
-              >
-                Personal Site
-              </a>
+            <div className="founder-container">
+              <img className="footer-img" src={propic} alt="jack propic"></img>
+              <div className="founder-info">
+                <p className="footer-name">Jack Cusick</p>
+                <a
+                  className="footer-linkedin-git"
+                  href="https://www.linkedin.com/in/jack-cusick-2a5809b4/"
+                  target="_blank"
+                >
+                  LinkedIn
+                </a>
+                <a
+                  className="footer-linkedin-git"
+                  href="https://github.com/jackcusick95"
+                  target="_blank"
+                >
+                  Github
+                </a>
+                <a
+                  className="footer-site"
+                  href="https://www.google.com/"
+                  target="_blank"
+                >
+                  Personal Site
+                </a>
+              </div>
             </div>
           </div>
           {/* <div className="splash-search">
